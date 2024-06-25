2613423d1a1d3c6160b5bc5c9d433512822114bd
TsvetanUsunov <usunov@olimex.com>
2024-06-25 15:54:01 +0300

    add copyright message to the sources

--- SOFTWARE/Neo6502pc-PWR-Firmware/Firmware/src/i2c_slave.c
+++ SOFTWARE/Neo6502pc-PWR-Firmware/Firmware/src/i2c_slave.c
@@ -1,3 +1,32 @@
+/* 
+ * Code for using the I2C peripheral in slave mode 
+ * for OLIMEX Neo6502PC-PWR board
+ * 
+ * Project is based on https://github.com/cnlohr/ch32v003fun/blob/master/examples/i2c_slave/
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2024 Renze Nicolai
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "i2c_slave.h"
 
 // Disable LOG_DEBUG locally
